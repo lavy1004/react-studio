15d0704f2121e5b23a3a2f861aa35bb8d15b2da9
1004wild <1004wild@naver.com>
2019-08-22 22:44:52 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '기환',
+  'birthday' : '920209',
+  'gender' : '남',
+  'job' : '프론트개발자',
+  },
+  {
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '홍길동',
+  'birthday' : '920209',
+  'gender' : '남',
+  'job' : '프론트개발자',
+  },
+  {
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '홍길동',
+  'birthday' : '920209',
+  'gender' : '남',
+  'job' : '프론트개발자',
+  },
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's learn management</h2>
-    </div>
+    <>
+    {
+      customers.map(c => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        )
+      })
+    }
+    </>
   );
 }
 
