35e32515bdba5d36f7b7333b3f19f5f5e0c693c3
1004wild <1004wild@naver.com>
2019-08-24 13:09:32 +0900

    aws , create database table & node.js database customer

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {withStyles} from '@material-ui/core/styles'
+import { TableRow } from '@material-ui/core';
 
 
 const styles= theme =>({
@@ -18,25 +20,37 @@ const styles= theme =>({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit *2
   }
 })
 
 
 
+
 class App extends React.Component{
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20)
     this.callApi()
       .then(res =>this.setState({customers: res}))
   }
 
+  //함수표현식인데
   callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
 
     return body 
+  }
+
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1})
   }
 
   render() {
@@ -66,13 +80,18 @@ class App extends React.Component{
               />
             )
           })
-          : ""
+          : 
+          <TableRow>
+            <TableCell colSpan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" vlaue={this.state.completed} />
+            </TableCell>
+          </TableRow>
           }</TableBody>
     </Table>
     
     </Paper>
   );
-        }
+}
 }
 
 export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,26 @@ const port = process.env.PORT ||5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+
+connection.connect();
+
 app.get('/api/customers', (req, res) =>{
-    res.send(
-        [
-            {
-            'id' : 1,
-            'image' : 'https://placeimg.com/64/64/1',
-            'name' : '기환',
-            'birthday' : '920209',
-            'gender' : '남',
-            'job' : '프론트개발자',
-            },
-            {
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/2',
-            'name' : '홍길동',
-            'birthday' : '920209',
-            'gender' : '남',
-            'job' : '프론트개발자',
-            },
-            {
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/3',
-            'name' : '홍길동',
-            'birthday' : '920209',
-            'gender' : '남',
-            'job' : '프론트개발자',
-            },
-        ]
+    connection.query(
+        'SELECT * FROM CUSTOMER',
+        (err, rows, fields) => {
+            res.send(rows);
+        }
     )
 })
 
